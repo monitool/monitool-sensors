f00ed6605e4dd213bf3e543784bab5b1579e3996
slawkow68 <slawkow68@wp.pl>
2015-03-28 10:41:04 +0100

    First commit with sensor libraries

---
+++ sensors/src/main/java/io/github/monitool/sensors/App.java
@@ -0,0 +1,13 @@
+package io.github.monitool.sensors;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

1833f5609c783699757ec9bc2a299e9d87e419de
slawkow68 <slawkow68@wp.pl>
2015-03-28 11:44:41 +0100

    Library for http requests; Scheduler;

--- sensors/src/main/java/io/github/monitool/sensors/App.java
+++ sensors/src/main/java/io/github/monitool/sensors/App.java
@@ -1,14 +1,26 @@
 package io.github.monitool.sensors;
 
+import java.io.IOException;
+
+//import it.sauronsoftware.cron4j.Scheduler;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
 
-        System.out.println( "Hello World!" );
-    }
+	public static void main(String[] args) {
+		MeasurementScheduler.startMeasurements("0/5 * * * * ?");
+		ClientHttp client = new ClientHttp();
+		try {
+			System.out.println(client.get("http://unirest.io/java.html"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println("Hello World!");
+
+	}
+
 }
---
+++ sensors/src/main/java/io/github/monitool/sensors/MeasurementScheduler.java
@@ -0,0 +1,31 @@
+package io.github.monitool.sensors;
+
+import org.quartz.CronScheduleBuilder;
+import org.quartz.Job;
+import org.quartz.JobBuilder;
+import org.quartz.JobDetail;
+import org.quartz.Scheduler;
+import org.quartz.SchedulerException;
+import org.quartz.Trigger;
+import org.quartz.TriggerBuilder;
+import org.quartz.impl.StdSchedulerFactory;
+
+public class MeasurementScheduler {
+	public static void startMeasurements(String cron) {
+		start(cron, MeasurementJob.class);
+	}
+
+	static void start(String cron, Class<? extends Job> jobClass) {
+		JobDetail job = JobBuilder.newJob(jobClass).build();
+		Trigger trigger = TriggerBuilder.newTrigger()
+				.withSchedule(CronScheduleBuilder.cronSchedule(cron)).build();
+		try {
+			Scheduler scheduler = new StdSchedulerFactory().getScheduler();
+			scheduler.start();
+			scheduler.scheduleJob(job, trigger);
+		} catch (SchedulerException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}

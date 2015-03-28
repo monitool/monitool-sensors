db7a3d28f44d0df5c97bb727aeb2c3c88fa3c962
ppalus <patrykpalus@gmail.com>
2015-03-28 12:57:33 +0100

    cpu and memory measurement

--- sensors/src/main/java/io/github/monitool/sensors/Monitor.java
+++ sensors/src/main/java/io/github/monitool/sensors/Monitor.java
@@ -2,39 +2,28 @@ package io.github.monitool.sensors;
 
 import java.lang.management.ManagementFactory;
 
-import com.jezhumble.javasysmon.JavaSysMon;
 import com.sun.management.OperatingSystemMXBean;
 
 public class Monitor {
 
-	private JavaSysMon sysMon;
 	OperatingSystemMXBean mxBean;
 
 	public Monitor() {
-		sysMon = new JavaSysMon();
 		mxBean = (OperatingSystemMXBean) ManagementFactory.getOperatingSystemMXBean();
 	}
 	
 	public Measure getMesure() {
 		Measure measure = new Measure();
-		getCpuUsage();
-		getMemoryUsage();
+		measure.setCpu(getCpuUsage());
+		measure.setMemory(getMemoryUsage());
 		return measure;
 	}
 	
 	private double getCpuUsage() {
-		System.out.println("sysMon cpu usage: " + sysMon.cpuTimes().getCpuUsage(sysMon.cpuTimes()));
-		System.out.println("mxBean cpu load: " + mxBean.getSystemCpuLoad());
-		
-		return -1.0;
+		return mxBean.getSystemCpuLoad();
 	}
 	
-	private double getMemoryUsage() {
-		System.out.println("sysMon free memory: " + sysMon.physical().getFreeBytes() / 1024);
-		System.out.println("sysMon used memory: " + sysMon.physical().getTotalBytes() / 1024);
-		System.out.println("mxBean free memory: " + mxBean.getFreePhysicalMemorySize() / 1024);
-		System.out.println("mxBean used memory: " + mxBean.getTotalPhysicalMemorySize() / 1024);
-		
-		return -1.0;
+	private double getMemoryUsage() {		
+		return mxBean.getFreePhysicalMemorySize() / mxBean.getTotalPhysicalMemorySize();
 	}
 }

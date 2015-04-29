69191a069c72b3aa78bfbbadec3525d4254f0ccb
slawkow68 <slawkow68@wp.pl>
2015-04-30 01:08:52 +0200

    One instance application

--- sensors/src/main/java/io/github/monitool/sensors/App.java
+++ sensors/src/main/java/io/github/monitool/sensors/App.java
@@ -1,23 +1,19 @@
 package io.github.monitool.sensors;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.apache.log4j.varia.NullAppender;
 
 public class App {
+	private static ServerSocket socket;
 
 	public static void main(String[] args) {
-		// try {
-		// ServerSocket socket = new ServerSocket(14999);
-		// Thread.sleep(1000);
-		// ServerSocket socket2 = new ServerSocket(14998);
+		oneInstanceAplication();
 		runAplication();
-		//
-		// } catch (java.net.BindException b) {
-		// System.out.println("Already Running...");
-		// } catch (Exception e) {
-		// System.out.println(e.toString());
-		// }
+
 	}
 
 	private static void runAplication() {
@@ -26,5 +22,31 @@ public class App {
 		SensorConfiguration.start();
 		MeasurementScheduler.startMeasurements(SensorConfiguration
 				.getInstance().getSendingCronExp());
+	}
+
+	private static void oneInstanceAplication() {
+		new Thread() {
+			@Override
+			public void run() {
+				try {
+					socket = new ServerSocket(12999);
+					while (true)
+						Thread.sleep(1000000000L);
+
+				} catch (IOException | InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+					System.exit(-1);
+				}
+			}
+
+		}.start();
+		while (socket == null || !socket.isBound())
+			try {
+				Thread.sleep(10L);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 	}
 }
